25152412cae858577dab8bf40df8fc1fdf744242
Farid Azmi <muhaziz@deloitte.com>
2025-02-06 11:10:07 +0700

    Optimize import

--- sns-publisher-service/src/main/java/com/imza/porto/snspublisher/controller/MessagePublisherRest.java
+++ sns-publisher-service/src/main/java/com/imza/porto/snspublisher/controller/MessagePublisherRest.java
@@ -8,15 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import software.amazon.awssdk.services.sns.SnsClient;
 
 import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/api/message")
